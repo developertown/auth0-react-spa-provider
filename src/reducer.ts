9f06bc74708e48b26660fa65d6c3b79a2ffd990f
Jason Vasquez <jason@developertown.com>
2020-03-25 13:11:28 -0400

    Simplification to reducer fn returns

--- src/reducer.ts
+++ src/reducer.ts
@@ -12,40 +12,29 @@ import {
 } from "./types";
 
 export const auth0ProviderStateReducer = reducerWithoutInitialState<Auth0ProviderState>()
-  .case(auth0Loaded, (state, params): AuthenticatedState | UnauthenticatedState => {
+  .case(auth0Loaded, (state, { user, auth0Client }): AuthenticatedState | UnauthenticatedState => {
     if (!isLoadingState(state)) {
       throw new Error("Invalid state: auth0Loaded may only be applied when in the loading state.");
     }
 
-    const commonValues = {
+    return {
       ...state,
 
       loading: false,
-      auth0Client: params.auth0Client,
+      auth0Client,
       popupOpen: false,
 
-      loginWithPopup: params.auth0Client.loginWithPopup,
-      loginWithRedirect: params.auth0Client.loginWithRedirect,
-      getTokenWithPopup: params.auth0Client.getTokenWithPopup,
-      getTokenSilently: params.auth0Client.getTokenSilently,
-      getIdTokenClaims: params.auth0Client.getIdTokenClaims,
-      handleRedirectCallback: params.auth0Client.handleRedirectCallback,
-      logout: params.auth0Client.logout,
-    };
+      loginWithPopup: auth0Client.loginWithPopup,
+      loginWithRedirect: auth0Client.loginWithRedirect,
+      getTokenWithPopup: auth0Client.getTokenWithPopup,
+      getTokenSilently: auth0Client.getTokenSilently,
+      getIdTokenClaims: auth0Client.getIdTokenClaims,
+      handleRedirectCallback: auth0Client.handleRedirectCallback,
+      logout: auth0Client.logout,
 
-    if (params.user) {
-      return {
-        ...commonValues,
-        user: params.user,
-        isAuthenticated: true,
-      } as AuthenticatedState;
-    } else {
-      return {
-        ...commonValues,
-        user: undefined,
-        isAuthenticated: false,
-      } as UnauthenticatedState;
-    }
+      user: user || undefined,
+      isAuthenticated: Boolean(user),
+    } as AuthenticatedState | UnauthenticatedState;
   })
   .case(loginWithPopupAction.started, (state) => {
     if (!isLoadedState(state)) {
@@ -57,27 +46,17 @@ export const auth0ProviderStateReducer = reducerWithoutInitialState<Auth0Provide
       popupOpen: true,
     };
   })
-  .case(loginWithPopupAction.done, (state, r): AuthenticatedState | UnauthenticatedState => {
+  .case(loginWithPopupAction.done, (state, { result: user }): AuthenticatedState | UnauthenticatedState => {
     if (!isLoadedState(state)) {
       throw new Error("Invalid state: loginWithPopupAction.done may only be applied when in a loaded state.");
     }
 
-    const { user } = r.result;
-    if (user) {
-      return {
-        ...state,
-        popupOpen: false,
-        user,
-        isAuthenticated: true,
-      };
-    } else {
-      return {
-        ...state,
-        popupOpen: false,
-        user,
-        isAuthenticated: false,
-      };
-    }
+    return {
+      ...state,
+      popupOpen: false,
+      user: user || undefined,
+      isAuthenticated: Boolean(user),
+    } as AuthenticatedState | UnauthenticatedState;
   })
   .case(handleRedirectCallbackAction.started, (state) => {
     if (!isLoadedState(state)) {
@@ -91,29 +70,18 @@ export const auth0ProviderStateReducer = reducerWithoutInitialState<Auth0Provide
       handlingRedirect: true,
     } as HandlingRedirectState;
   })
-  .case(handleRedirectCallbackAction.done, (state, r): AuthenticatedState | UnauthenticatedState => {
+  .case(handleRedirectCallbackAction.done, (state, { result: user }): AuthenticatedState | UnauthenticatedState => {
     if (!isHandlingRedirectState(state)) {
       throw new Error(
         "Invalid state: handleRedirectCallbackAction.done may only be applied when in the handlingRedirect state.",
       );
     }
 
-    const { user } = r.result;
-
-    if (user) {
-      return {
-        ...state,
-        user,
-        isAuthenticated: true,
-        handlingRedirect: false,
-      };
-    } else {
-      return {
-        ...state,
-        user: undefined,
-        isAuthenticated: false,
-        handlingRedirect: false,
-      };
-    }
+    return {
+      ...state,
+      user,
+      isAuthenticated: Boolean(user),
+      handlingRedirect: false,
+    } as AuthenticatedState | UnauthenticatedState;
   })
   .build();
